2129f39eb43ec801214cec65bef5c9a14cb30034
Emmanuel Sackey <eboriley@hotmail.co.uk>
2022-06-19 18:58:16 +0000

    solo player completed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,70 @@
+const optionMenu = document.querySelector('.game_start');
+const gameBoard = document.querySelector('.game_board');
 const iconContainer = document.querySelector('.icons');
+const btnIcons = document.getElementById('btn_icons');
+const btnNumbers = document.getElementById('btn_numbers');
+const btn1 = document.getElementById('btn_1');
+const btn2 = document.getElementById('btn_2');
+const btn3 = document.getElementById('btn_3');
+const btn4 = document.getElementById('btn_4');
+const btn4x4 = document.getElementById('btn_4x4');
+const btn6x6 = document.getElementById('btn_6x6');
+const btnStartGame = document.getElementById('btn_start_game');
+const restartGame = document.getElementById('restart');
+const btnGameEndsRestart = document.getElementById(
+  'game_end_restart'
+);
+const btnGameEndsNewGame = document.getElementById(
+  'game_end_new_game'
+);
+let theme = 'images';
+let players = 1;
+let grid = '4x4';
+
+btnIcons.addEventListener('click', () => {
+  theme = 'icons';
+  document.querySelectorAll('.btn_theme').forEach((item) => {
+    if (item.classList.contains('game_start-active')) {
+      item.classList.remove('game_start-active');
+    }
+  });
+  btnIcons.classList.add('game_start-active');
+});
+
+btnNumbers.addEventListener('click', () => {
+  theme = 'numbers';
+  document.querySelectorAll('.btn_theme').forEach((item) => {
+    if (item.classList.contains('game_start-active')) {
+      item.classList.remove('game_start-active');
+    }
+  });
+  btnNumbers.classList.add('game_start-active');
+});
+
+btn1.addEventListener('click', () => {
+  players = 1;
+  console.log(players);
+});
+
+btn4x4.addEventListener('click', () => {
+  grid = '4x4';
+  document.querySelectorAll('.btn_grid').forEach((item) => {
+    if (item.classList.contains('game_start-active')) {
+      item.classList.remove('game_start-active');
+    }
+  });
+  btn4x4.classList.add('game_start-active');
+});
+
+btn6x6.addEventListener('click', () => {
+  grid = '6x6';
+  document.querySelectorAll('.btn_grid').forEach((item) => {
+    if (item.classList.contains('game_start-active')) {
+      item.classList.remove('game_start-active');
+    }
+  });
+  btn6x6.classList.add('game_start-active');
+});
 
 const images = [
   `<div class="icon"><i class="fas fa-cat"></i></div>`,
@@ -20,9 +86,6 @@ const images = [
   `<div class="icon"><i class="fab fa-spotify"></i></div>`,
   `<div class="icon"><i class="fas fa-tint"></i></div>`,
 ];
-
-let theme = 'images';
-let smallerGrid = true;
 
 const numbers = [
   `<div class="icon numbers"><p class="num-0">0</p></div>`,
@@ -75,45 +138,46 @@ const generateImagesRandom = (arr) => {
   return random;
 };
 
-const gridSize = getRandomNumber(8, 18);
+btnStartGame.addEventListener('click', () => {
+  optionMenu.classList.add('hidden');
+  gameBoard.classList.remove('hidden');
+  const gridSize = getRandomNumber(8, 18);
 
-let imagepairs;
+  let imagepairs;
+  loadGame(gridSize, imagepairs);
+});
 
-if (smallerGrid) {
-  imagepairs =
-    theme === 'images'
-      ? generateImagesRandom(
-          duplicateImages(images.slice(gridSize - 8, gridSize))
-        )
-      : generateImagesRandom(
-          duplicateImages(numbers.slice(gridSize - 8, gridSize))
-        );
-} else {
-  imagepairs =
-    theme === 'images'
-      ? generateImagesRandom(duplicateImages(images))
-      : generateImagesRandom(duplicateImages(numbers));
-}
+btnGameEndsRestart.addEventListener('click', () => {
+  clearContainer(iconContainer);
+  timeElapsed = '';
+  movesCount = 0;
+  pairedIcons = 0;
+  min = 0;
+  sec = 0;
+  document.getElementById('moves').innerText = '';
+  document.getElementById('time').innerText = '';
+  document.querySelector('.game_end').classList.add('hidden');
+  optionMenu.classList.add('hidden');
+  gameBoard.classList.remove('hidden');
+  const gridSize = getRandomNumber(8, 18);
 
-//append all images to image container
-for (pairs of imagepairs) {
-  iconContainer.innerHTML += pairs;
-}
+  let imagepairs;
+  loadGame(gridSize, imagepairs);
+});
 
-if (smallerGrid) {
-  document.querySelectorAll('.icon').forEach((item) => {
-    item.style.width = '40px';
-    item.style.height = '40px';
-    item.style.fontsize = '1.6rem';
-  });
-}
-
-setTimeout(() => {
-  document.querySelectorAll('.icon').forEach((item) => {
-    item.classList.add('cover');
-    item.firstChild.classList.add('cover_text');
-  });
-}, 2000);
+btnGameEndsNewGame.addEventListener('click', () => {
+  clearContainer(iconContainer);
+  timeElapsed = '';
+  movesCount = 0;
+  pairedIcons = 0;
+  min = 0;
+  sec = 0;
+  document.getElementById('moves').innerText = '';
+  document.getElementById('time').innerText = '';
+  document.querySelector('.game_end').classList.add('hidden');
+  optionMenu.classList.remove('hidden');
+  gameBoard.classList.add('hidden');
+});
 
 //add event listeners to all images
 let clickedImage = [];
@@ -121,49 +185,108 @@ let isMatchedCards;
 let isNotMatchedCards;
 let movesCount = 0;
 let pairedIcons = 0;
+let isGameOver;
 let gameOver = false;
-document.querySelectorAll('.icon').forEach((item) => {
-  item.addEventListener('click', () => {
-    //reset the arr holding last selected images
+let hasGameBegan = false;
+let timeElapsed = '';
 
-    if (clickedImage.length == 1) {
-      clearTimeout(isMatchedCards);
-      clearTimeout(isNotMatchedCards);
-    }
-
-    movesCount++;
-    document.getElementById('moves').innerText = movesCount;
-
-    //display card image when the card is selected
-    item.classList.remove('cover');
-    item.classList.add('selected');
-    item.classList.add('disabledbtntemp');
-    item.firstChild.classList.add('disabledbutton_text');
-    if (clickedImage.length <= 2) {
-      clickedImage.push(item.firstChild.classList.value);
-    }
-
-    //logic after two cards are selected
-    if (clickedImage.length === 2) {
-      iconContainer.classList.add('disabledbtntemp');
-      //if the images on selected cards match
-      isMatchedCards = setTimeout(cardMatched, 500, clickedImage);
-
-      //if the images on the cards do not match
-      isNotMatchedCards = setTimeout(
-        cardNotMatched,
-        500,
-        clickedImage
-      );
-
-      if ((isMatchedCards, isNotMatchedCards)) {
-        clickedImage = [];
-      }
-    }
-  });
+restartGame.addEventListener('click', () => {
+  alert('!!!!!!!!!!');
 });
 
-function cardMatched(clickedImage) {
+function loadGame(gridSize, imagepairs) {
+  if (grid === '4x4') {
+    imagepairs =
+      theme === 'icons'
+        ? generateImagesRandom(
+            duplicateImages(images.slice(gridSize - 8, gridSize))
+          )
+        : generateImagesRandom(
+            duplicateImages(numbers.slice(gridSize - 8, gridSize))
+          );
+  } else {
+    imagepairs =
+      theme === 'icons'
+        ? generateImagesRandom(duplicateImages(images))
+        : generateImagesRandom(duplicateImages(numbers));
+  }
+
+  //append all images to image container
+  for (pairs of imagepairs) {
+    iconContainer.innerHTML += pairs;
+  }
+
+  if (grid === '4x4') {
+    document.querySelectorAll('.icon').forEach((item) => {
+      item.style.width = '55px';
+      item.style.height = '55px';
+      item.style.fontsize = '1.6rem';
+    });
+  }
+  iconContainer.classList.add('disabledbtntemp');
+  setTimeout(showCardsTemp, 2000);
+
+  gameControl(imagepairs);
+}
+
+function showCardsTemp() {
+  document.querySelectorAll('.icon').forEach((item) => {
+    item.classList.add('cover');
+    item.firstChild.classList.add('cover_text');
+  });
+  iconContainer.classList.remove('disabledbtntemp');
+  isGameOver = setInterval(timer, 1000);
+}
+
+//handle click on each card
+function gameControl(imagepairs) {
+  document.querySelectorAll('.icon').forEach((item) => {
+    item.addEventListener('click', () => {
+      //reset the arr holding last selected images
+      if (clickedImage.length == 1) {
+        clearTimeout(isMatchedCards);
+        clearTimeout(isNotMatchedCards);
+      }
+
+      movesCount++;
+      document.getElementById('moves').innerText = movesCount;
+
+      //display card image when the card is selected
+      item.classList.remove('cover');
+      item.classList.add('selected');
+      item.classList.add('disabledbtntemp');
+      item.firstChild.classList.add('disabledbutton_text');
+      if (clickedImage.length <= 2) {
+        clickedImage.push(item.firstChild.classList.value);
+      }
+
+      //logic after two cards are selected
+      if (clickedImage.length === 2) {
+        iconContainer.classList.add('disabledbtntemp');
+        //if the images on selected cards match
+        isMatchedCards = setTimeout(
+          cardMatched,
+          500,
+          clickedImage,
+          imagepairs
+        );
+
+        //if the images on the cards do not match
+        isNotMatchedCards = setTimeout(
+          cardNotMatched,
+          500,
+          clickedImage
+        );
+
+        if ((isMatchedCards, isNotMatchedCards)) {
+          clickedImage = [];
+        }
+      }
+    });
+  });
+}
+
+function cardMatched(clickedImage, imagepairs) {
   if (clickedImage[0] === clickedImage[1]) {
     document.querySelectorAll('.icon').forEach((item) => {
       if (
@@ -183,6 +306,11 @@ function cardMatched(clickedImage) {
   if (pairedIcons === imagepairs.length) {
     gameOver = true;
     if (gameOver) clearInterval(isGameOver);
+    document.getElementById('time-elapsed').innerText = timeElapsed;
+    document.getElementById('moves-taken').innerText = movesCount;
+    document.querySelector('.game_end').classList.remove('hidden');
+    console.log('time elapsed ' + timeElapsed);
+    console.log('moves taken ' + movesCount);
   }
 }
 
@@ -202,19 +330,25 @@ function cardNotMatched(clickedImage) {
     });
   }
 }
+
+function clearContainer(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
 let min = 0;
 let sec = 0;
 function timer() {
   sec++;
   if (sec < 10) {
-    document.getElementById('time').innerText = `${min}:0${sec}`;
+    timeElapsed = `${min}:0${sec}`;
+    document.getElementById('time').innerText = timeElapsed;
   } else {
-    document.getElementById('time').innerText = `${min}:${sec}`;
+    timeElapsed = `${min}:${sec}`;
+    document.getElementById('time').innerText = timeElapsed;
   }
   if (sec >= 59) {
     sec = -1; //because we want the next count to begin from zero
     min++;
   }
 }
-
-const isGameOver = setInterval(timer, 1000);
